a6b08f721eaccac5acfd0e552a911c20a1f46745
Dan <a125012239@gmail.com>
2024-06-09 00:20:31 +0800

    init

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+
+function loadEndpoints() {
+    const configPath = './config/endpoint.json';
+    const endpointsConfig = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+
+    endpointsConfig.endpoints.forEach(endpoint => {
+        const basePath = path.join('./config', endpoint.path);
+        const responseConfig = JSON.parse(fs.readFileSync(path.join(basePath, 'response.json'), 'utf8'));
+        const requestConfig = JSON.parse(fs.readFileSync(path.join(basePath, 'request.json'), 'utf8'));
+        //const headerConfig = JSON.parse(fs.readFileSync(path.join(basePath, 'header.json'), 'utf8'));
+
+        app[endpoint.method](endpoint.path, (req, res) => {
+
+            // if (!validateHeaders(req.headers, headerConfig)) {
+            //     console.log('Request Headers:', req.headers);
+            //     return res.status(400).send({ error: "Invalid headers" });
+            // }
+
+            if (!validateBody(req.body, requestConfig.bodyStructure)) {
+                console.log('Request Body:', req.body);
+                return res.status(400).send({ error: "Invalid request body" });
+            }
+
+            res.status(responseConfig.status).json(responseConfig.body);
+        });
+    });
+}
+
+function validateHeaders(headers, requiredHeaders) {
+    return Object.keys(requiredHeaders).every(key => headers[key] && headers[key] === requiredHeaders[key]);
+}
+
+function validateBody(body, structure) {
+    function validateField(body, structure) {
+        return Object.keys(structure).every(key => {
+            const spec = structure[key];
+            if (typeof spec === 'object' && !Array.isArray(spec) && spec !== null) {
+                if (spec.type === undefined) {
+                    return key in body && validateField(body[key], spec);
+                } else {
+                    if (spec.isRequired && !(key in body)) {
+                        return false;
+                    }
+                    if (key in body) {
+                        if ('type' in spec) {
+                            if (spec.type === "Date") {
+                                body[key] = new Date(body[key])
+                                if (!(body[key] instanceof Date)) {
+                                    return false;
+                                }
+                            } else if (typeof body[key] !== spec.type) {
+                                return false;
+                            }
+                        }
+                        if ('equals' in spec && spec.equals !== null && body[key] !== spec.equals) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            } else {
+                return true;
+            }
+        });
+    }
+    return validateField(body, structure);
+}
+
+
+
+loadEndpoints();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
